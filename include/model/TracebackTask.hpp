5abb0a75cae186ce0606afc763abf367b0be6199
Raiden Shogun <tangjiaming1999@gmail.com>
2023-12-02 20:16:19 +0100

    feat: add communication object for tracebacks

--- include/model/TracebackTask.hpp
+++ include/model/TracebackTask.hpp
@@ -4,16 +4,31 @@
 
 class TracebackTask : public AbstratJsonObject {
    public:
-    virtual std::string toJson(){
-
+    virtual std::string toJson() {
+        nlohmann::json j;
+        j["type"] = "TracebackTask";
+        j["x"] = x_;
+        j["y"] = y_;
+        j["startX"] = start_x_;
+        j["startY"] = start_y_;
+        return j.dump();
     }
     virtual void loadFromJsonObject(const nlohmann::json& j) {
-
+        x_ = j["x"].template get<int>();
+        y_ = j["y"].template get<int>();
+        start_x_ = j["startX"].template get<int>();
+        start_y_ = j["startY"].template get<int>();
     }
 
-    public:
-
-    
+   public:
+    // block row number
+    int x_;
+    // block column number
+    int y_;
+    // where traceback starts
+    int start_x_;
+    // where traceback ends
+    int start_y_;
 };
 
 #endif
--- include/model/TracebackTaskResponse.hpp
+++ include/model/TracebackTaskResponse.hpp
@@ -4,16 +4,35 @@
 
 class TracebackTaskResponse : public AbstratJsonObject {
    public:
-    virtual std::string toJson(){
-
+    virtual std::string toJson() {
+        nlohmann::json j;
+        j["type"] = "TracebackTaskResponse";
+        j["x"] = x_;
+        j["y"] = y_;
+        j["endX"] = end_x_;
+        j["endY"] = end_y_;
+        j["sequence"] = sequence_;
+        return j.dump();
     }
     virtual void loadFromJsonObject(const nlohmann::json& j) {
-
+        x_ = j["x"].template get<int>();
+        y_ = j["y"].template get<int>();
+        end_x_ = j["endX"].template get<int>();
+        end_y_ = j["endY"].template get<int>();
+        sequence_ = j["sequence"].template get<std::string>();
     }
 
-    public:
-
-    
+   public:
+    // returned sequence
+    std::string sequence_;
+    // block row number
+    int x_;
+    // block column number
+    int y_;
+    // location where the traceback ends
+    int end_x_;
+    // location where the traceback ends
+    int end_y_;
 };
 
 #endif
